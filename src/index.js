0713137ea11c5757e267320c463cd96afb5f1209
Lavrov Andrey <Sattarlav@gmail.com>
2022-08-31 15:11:28 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { fetchImages, resetPage } from "./js/fetchImages";
 
 let searchQuery = '';
+
 const refs = {
   form: document.querySelector(".search-form"),
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
 }
 
+
 refs.form.addEventListener('submit', onSubmitClick);
 refs.loadMore.addEventListener('click', onLoadMoreClick);
 
 function onSubmitClick(e) {
-    e.preventDefault();
+  e.preventDefault();
+  clearGallery();
+  refs.loadMore.disabled = false;
   searchQuery = e.target.elements.searchQuery.value.trim();
   resetPage();
-  fetchImages(searchQuery).then(renderSearchQuery);
+
+  fetchImages(searchQuery).then(data => {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    return data;
+  }).then(renderSearchQuery);
 }
 
 function onLoadMoreClick() {
-  fetchImages(searchQuery).then(renderSearchQuery);
+  fetchImages(searchQuery).then(data => {
+    return data
+  }).then(renderSearchQuery).catch(() => {
+    Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+    refs.loadMore.disabled = true;
+  });
 }
 
 
 
 function renderSearchQuery(queries) {
-    const markup = queries.map(({ tags, webformatURL, likes, views, comments, downloads }) => `<div class="photo-card">
+  console
+    const markup = queries.hits.map(({ tags, webformatURL, likes, views, comments, downloads }) => `<div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
@@ -51,3 +66,6 @@ function renderSearchQuery(queries) {
    refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
 
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,8 +1,10 @@
+import Notiflix, { Loading } from 'notiflix';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '?key=29607752-b3abb9abc1baaf34c2e91fe2c';
 const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true'
 let page = 1;
-let perPage = 3;
+let perPage = 200;
 
 function fetchImages(name) {
     return fetch(`${BASE_URL}${API_KEY}&q=${name}&${OPTIONS}&page=${page}&per_page=${perPage}`).then(response => {
@@ -12,8 +14,13 @@ function fetchImages(name) {
         }
         return response.json();
     }).then(data => {
+        
+        if (data.totalHits === 0) {
+           return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        }
         page += 1;
-        return data.hits;
+        return data;
+        
     })
 }
 
@@ -23,4 +30,3 @@ function resetPage() {
 
 
 export { fetchImages, resetPage };
-
